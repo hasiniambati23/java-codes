c39c29ac7966bbd99df9bd9f9f7f293fb7b02b60
Hasini Reddy Ambati <hasiniambati200@gmail.com>
2026-01-30 08:24:22 +0530

    Initial commit

---
+++ Substrings.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Substrings {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String email;
+
+        while (true) {
+            System.out.print("Enter email: ");
+            email = sc.nextLine();
+
+            if (email.contains("@")) {
+                break; // valid email → exit loop
+            } else {
+                System.out.println("Your mail does not contain @. Please enter again.");
+            }
+        }
+
+        String username = email.substring(0, email.indexOf("@"));
+        System.out.println("Your username is: " + username);
+
+        sc.close();
+    }
+}
